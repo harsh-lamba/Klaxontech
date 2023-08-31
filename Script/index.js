a45554705c2035bf91c6324b857f089f3c65a913
Akhilesh Kamat <akhileshkamat1999dpt@gmail.com>
2023-08-31 21:17:56 +0530

    Initial commit

---
+++ Script/index.js
@@ -0,0 +1,39 @@
+const icons = document.querySelectorAll(".down-icon");
+const listItems = document.querySelectorAll(".list-item");
+const listItemWrappers = document.querySelectorAll(".list-item-wrapper");
+const dropDowns = document.querySelectorAll(".dropdown");
+const cross = document.querySelector(".cross");
+const mobileNav1 = document.querySelector(".mobile-nav-main-wrapper");
+const overlay = document.querySelector(".overlay");
+const arrow = document.querySelector(".arrow-wrapper");
+const mList = document.querySelectorAll(".m-list");
+const progressBar = document.querySelector(".nav-roller-progress-bar");
+const mListContent = document.querySelectorAll(".sub-items-wrapper");
+const hamburger = document.querySelector("#hamburger");
+const body = document.querySelector("#body");
+const mobileNav = document.querySelector(".mobile-nav");
+
+// selecting google translate elements
+
+// Get the target element by its ID
+const targetElement = document.querySelector(".goog-te-gadget")
+
+// Check if the target element exists
+
+const watiForElem = ()=>{
+
+    // if (targetElement) {
+    //   // Find the text node containing "Powered by" and remove it
+    //   targetElement.childNodes.forEach(childNode => {
+    //     if (childNode.nodeType === Node.TEXT_NODE && childNode.textContent.includes("Powered by")) {
+    //       childNode.textContent = childNode.textContent.replace("Powered by", "");
+    //     }
+    //   });
+    // }
+    console.log(targetElement)
+}
+
+setTimeout(watiForElem,5000)
+
+
+// console.log(rmvTxt)
---
+++ Script/navbar.js
@@ -0,0 +1,111 @@
+
+
+const toggleNav = ()=>{
+
+
+listItemWrappers.forEach((wrapper, wrapperIndex) => {
+    wrapper.addEventListener("click", () => {
+        listItems.forEach((li, liIndex) => {
+            const isActive = liIndex === wrapperIndex;
+
+            if(isActive){
+                li.classList.toggle("active");
+            }
+            else{
+                li.classList.remove("active")
+            }
+            
+        });
+
+        icons.forEach((icon, iconIndex) => {
+            const isActive = iconIndex === wrapperIndex;
+            if(isActive){
+                icon.classList.toggle("active");
+            }
+            else{
+                icon.classList.remove("active")
+            }
+        });
+
+        dropDowns.forEach((dropDown, dIndex)=>{
+            const isActive = dIndex === wrapperIndex;
+            if(isActive){
+                dropDown.classList.toggle("active-nav-animation")
+            }
+            else{
+                dropDown.classList.remove("active-nav-animation")
+            }
+        })
+    });
+});
+}
+
+toggleNav()
+// toggle hamburger
+
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.add("hide")
+    mobileNav.classList.add("active")
+    mobileNav1.classList.add("active")
+    cross.classList.remove("hide")
+    overlay.classList.add("active")
+    body.classList.add("overflow-hidden")
+
+})
+cross.addEventListener("click",()=>{
+    cross.classList.add("hide")
+    hamburger.classList.remove("hide")
+    mobileNav.classList.remove("active")
+    mobileNav1.classList.remove("active")
+    overlay.classList.remove("active")
+    body.classList.remove("overflow-hidden")
+})
+
+
+
+
+
+mList.forEach((list,index)=>{
+    list.addEventListener("click", ()=>{
+        mListContent.forEach((mLi,midex)=>{
+            const isActive = midex === index;
+            if(isActive){
+                mLi.classList.toggle("active")
+            }
+        })
+    })
+})
+
+// arrow rotate
+
+
+const scrollProgressBar = () => {
+
+     let h = document.documentElement;
+     let st = h.scrollTop || document.body.scrollTop;
+     let sh = h.scrollHeight || document.body.scrollHeight;
+     let percent = st / (sh - h.clientHeight)*100;
+     let roundedPercent = Math.round(percent);
+    progressBar.style.width = `${roundedPercent}%`;
+    console.log(progressBar)
+
+};
+
+
+window.addEventListener("scroll",(e)=>{
+
+    scrollProgressBar();
+
+    if(window.scrollY > 0){
+        arrow.classList.add("active")
+    }
+    else{
+        arrow.classList.remove("active")
+    }
+
+})
+
+arrow.addEventListener("click",()=>{
+    window.scrollTo(0, 0);
+})
+
