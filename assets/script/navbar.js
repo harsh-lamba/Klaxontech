74f512a17206af32ec484d7f964bef8a581c21a8
Harsh K. Lamba <harsh.k.lamba@iongroup.com>
2024-05-05 20:31:39 +0530

    ion icons replaces with font awesome

--- assets/script/navbar.js
+++ assets/script/navbar.js
@@ -1,49 +1,47 @@
 
 
-const toggleNav = ()=>{
+const toggleNav = () => {
+    listItemWrappers.forEach((wrapper, wrapperIndex) => {
+        wrapper.addEventListener("click", () => {
+            listItems.forEach((li, liIndex) => {
+                const isActive = liIndex === wrapperIndex;
 
+                if (isActive) {
+                    li.classList.toggle("active");
+                }
+                else {
+                    li.classList.remove("active")
+                }
 
-listItemWrappers.forEach((wrapper, wrapperIndex) => {
-    wrapper.addEventListener("click", () => {
-        listItems.forEach((li, liIndex) => {
-            const isActive = liIndex === wrapperIndex;
+            });
 
-            if(isActive){
-                li.classList.toggle("active");
-            }
-            else{
-                li.classList.remove("active")
-            }
-            
+            icons.forEach((icon, iconIndex) => {
+                const isActive = iconIndex === wrapperIndex;
+                if (isActive) {
+                    icon.classList.toggle("active");
+                }
+                else {
+                    icon.classList.remove("active")
+                }
+            });
+
+            dropDowns.forEach((dropDown, dIndex) => {
+                const isActive = dIndex === wrapperIndex;
+                if (isActive) {
+                    dropDown.classList.toggle("active-nav-animation")
+                }
+                else {
+                    dropDown.classList.remove("active-nav-animation")
+                }
+            })
         });
-
-        icons.forEach((icon, iconIndex) => {
-            const isActive = iconIndex === wrapperIndex;
-            if(isActive){
-                icon.classList.toggle("active");
-            }
-            else{
-                icon.classList.remove("active")
-            }
-        });
-
-        dropDowns.forEach((dropDown, dIndex)=>{
-            const isActive = dIndex === wrapperIndex;
-            if(isActive){
-                dropDown.classList.toggle("active-nav-animation")
-            }
-            else{
-                dropDown.classList.remove("active-nav-animation")
-            }
-        })
     });
-});
 }
 
 toggleNav()
 // toggle hamburger
 
-hamburger.addEventListener("click",()=>{
+hamburger.addEventListener("click", () => {
     hamburger.classList.add("hide")
     mobileNav.classList.add("active")
     mobileNav1.classList.add("active")
@@ -52,7 +50,7 @@ hamburger.addEventListener("click",()=>{
     body.classList.add("overflow-hidden")
 
 })
-cross.addEventListener("click",()=>{
+cross.addEventListener("click", () => {
     cross.classList.add("hide")
     hamburger.classList.remove("hide")
     mobileNav.classList.remove("active")
@@ -65,11 +63,11 @@ cross.addEventListener("click",()=>{
 
 
 
-mList.forEach((list,index)=>{
-    list.addEventListener("click", ()=>{
-        mListContent.forEach((mLi,midex)=>{
+mList.forEach((list, index) => {
+    list.addEventListener("click", () => {
+        mListContent.forEach((mLi, midex) => {
             const isActive = midex === index;
-            if(isActive){
+            if (isActive) {
                 mLi.classList.toggle("active")
             }
         })
@@ -81,33 +79,33 @@ mList.forEach((list,index)=>{
 
 const scrollProgressBar = () => {
 
-     let h = document.documentElement;
-     let st = h.scrollTop || document.body.scrollTop;
-     let sh = h.scrollHeight || document.body.scrollHeight;
-     let percent = st / (sh - h.clientHeight)*100;
-     let roundedPercent = Math.round(percent);
+    let h = document.documentElement;
+    let st = h.scrollTop || document.body.scrollTop;
+    let sh = h.scrollHeight || document.body.scrollHeight;
+    let percent = st / (sh - h.clientHeight) * 100;
+    let roundedPercent = Math.round(percent);
     progressBar.style.width = `${roundedPercent}%`;
     // console.log(progressBar)
 
 };
 
 
-window.addEventListener("scroll",(e)=>{
+window.addEventListener("scroll", (e) => {
 
     scrollProgressBar();
 
-    if(window.scrollY > 0){
+    if (window.scrollY > 0) {
         arrow.classList.add("active")
         document.querySelector(".arrow-wrapper").style.opacity = `1`;
     }
-    else{
+    else {
         arrow.classList.remove("active")
         document.querySelector(".arrow-wrapper").style.opacity = `0`;
     }
 
 })
 
-arrow.addEventListener("click",()=>{
+arrow.addEventListener("click", () => {
     window.scrollTo(0, 0);
 })
 
