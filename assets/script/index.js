0b64ecb9da263dc7f42414da9520ea11a91d248c
Harsh K. Lamba <harsh.k.lamba@iongroup.com>
2024-05-17 01:26:45 +0530

    new form added

--- assets/script/index.js
+++ assets/script/index.js
@@ -96,11 +96,42 @@ const targetElement = document.querySelector(".goog-te-gadget")
 
 // Check if the target element exists
 
-document.getElementById("myForm").addEventListener("redirect", function (event) {
-    event.preventDefault(); // Prevent default form submission behavior
-    // Perform any form validation here if needed
+const form = document.getElementById('contactus');
+const result = document.getElementById('result');
 
-    // Redirect to a new route
-    window.location.href = "/new-route";
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: json
+    })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = "Form submitted successfully";
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function () {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
 });
 
